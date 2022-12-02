4ce485742f0b3c64d4098f69562c24c59a9d9071
Hugo KELFANI <hugo.kelfani@gmail.com>
2022-12-02 14:23:49 +0100

    Make it a little bit more readable.

--- solutions.js
+++ solutions.js
@@ -1,9 +1,28 @@
 function advent_1_1(input) {
-    return Math.max(...input.split('\n\n').map(e => e.split('\n').map(s => parseInt(s)).reduce((a, c) => a + c, 0)))
+    return Math.max(...
+        input
+            .split('\n\n')
+            .map(elf => 
+                elf
+                    .split('\n')
+                    .map(s => parseInt(s))
+                    .reduce((a, c) => a + c, 0)
+            )
+    )
 }
 
 function advent_1_2(input) {
-    return input.split('\n\n').map(e => e.split('\n').map(s => parseInt(s)).reduce((a, c) => a + c, 0)).sort((a, b) => a - b).slice(-3).reduce((a, c) => a + c, 0)
+    return input
+        .split('\n\n')
+        .map(elf => 
+            elf
+                .split('\n')
+                .map(s => parseInt(s))
+                .reduce((a, c) => a + c, 0)
+        )
+        .sort((a, b) => a - b)
+        .slice(-3)
+        .reduce((a, c) => a + c, 0);
 }
 
 function advent_2_1(guide) {

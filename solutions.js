f54c7800876aaabc60e26109568222262a86f382
Hugo KELFANI <hugo.kelfani@gmail.com>
2022-12-05 12:12:39 +0100

    fix indentation

--- solutions.js
+++ solutions.js
@@ -129,65 +129,65 @@ function advent_5_parse_stock(input) {
 
 	input
 		.split('\n')
-        .map(line => line + ' ')
+		.map(line => line + ' ')
 		.filter(line => line.includes('['))
 		.reverse()
 		.forEach(line => {
 			let n;
 
-            while ((n = regex.exec(line)) !== null) {
+			while ((n = regex.exec(line)) !== null) {
 				if (n.index === regex.lastIndex) {
 					regex.lastIndex++;
 				}
 
-                const stack_index = ~~(n.index / 4) + 1;
+				const stack_index = ~~(n.index / 4) + 1;
 
-                if (n[0].trim().length === 0) continue;
+				if (n[0].trim().length === 0) continue;
 
-                if (!stock[stack_index]) stock[stack_index] = [];
+				if (!stock[stack_index]) stock[stack_index] = [];
 
-                stock[stack_index].push(n[1]);
+				stock[stack_index].push(n[1]);
 			}
 		});
 
-    return stock;
+	return stock;
 }
 
 function advent_5_parse_commands(input) {
-    return input
-        .split('\n')
-        .filter(line => line.includes('move'))
-        .map(line => { 
-            const captures = line.match(/move (\d+) from (\d+) to (\d+)/);
-            return {
-                qty: captures[1],
-                from: captures[2],
-                to: captures[3]
-            }
-        });
+	return input
+		.split('\n')
+		.filter(line => line.includes('move'))
+		.map(line => { 
+			const captures = line.match(/move (\d+) from (\d+) to (\d+)/);
+			return {
+				qty: captures[1],
+				from: captures[2],
+				to: captures[3]
+			}
+		});
 }
 
 function advent_5_1(input) {
-    let stock = advent_5_parse_stock(input)
+	let stock = advent_5_parse_stock(input)
 
-    advent_5_parse_commands(input)
-        .forEach(instruction => {
-            for (let i = 0; i < instruction.qty; ++i) {
-                stock[instruction.to].push(stock[instruction.from].pop())
-            }
-        });
-    
-    return stock.reduce((acc, cur) => acc + cur.pop(), '')
+	advent_5_parse_commands(input)
+		.forEach(instruction => {
+			for (let i = 0; i < instruction.qty; ++i) {
+				stock[instruction.to].push(stock[instruction.from].pop())
+			}
+		});
+	
+	return stock.reduce((acc, cur) => acc + cur.pop(), '')
 }
 
 function advent_5_2(input) {
-    let stock = advent_5_parse_stock(input);
+	let stock = advent_5_parse_stock(input);
 
-    advent_5_parse_commands(input)
-        .forEach(instruction => {
-            stock[instruction.to] = stock[instruction.to].concat(stock[instruction.from].slice(-instruction.qty));
-            stock[instruction.from] = stock[instruction.from].slice(0, -instruction.qty);
-        });
-    
-    return stock.reduce((acc, cur) => acc + cur.pop(), '')
+	advent_5_parse_commands(input)
+		.forEach(instruction => {
+			stock[instruction.to] = stock[instruction.to].concat(stock[instruction.from].slice(-instruction.qty));
+			stock[instruction.from] = stock[instruction.from].slice(0, -instruction.qty);
+		});
+	
+	return stock.reduce((acc, cur) => acc + cur.pop(), '')
 }
